523be50c9af577cec85249a41ef49556a67b8ec3
paige <impaigeharris@gmail.com>
2021-12-04 16:10:43 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83ac6b00a5a50db5c14f17ac37851f89.js"
+  "/precache-manifest.cb415af5c78b476226536af38f17d939.js"
 );
 
 self.addEventListener('message', (event) => {
