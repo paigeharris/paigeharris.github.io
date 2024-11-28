cdd9b1b0f990d7437f92f279349b81ce52251206
Paige Harris <impaigeharris@gmail.com>
2024-11-27 21:32:31 -0800

    PBH165
    Use paigeharris.github.io as public health final page

--- src/db/orm.utils.js
+++
@@ -1,9 +0,0 @@
-import { createSelector as createORMSelector } from 'redux-orm';
-import orm from "./orm"
-
-export const createSelector = (...rest) =>
-  createORMSelector(
-    orm,
-    state => state.db,
-    (state, props) => props, ...rest
-  );
